001010dcb42f0c772ec6d7e1a488de07bccdfc12
cwill83247 <breakthrumusic@hotmail.com>
2022-12-16 15:28:57 +0000

    Add: Adding view and template to show data

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(null=False, blank=False,default=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):                  # overriding the default django in the table display name now rather than the ID
+        return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from .models import Item
 
 # Create your views here.
+
+
 def get_todo_list(request):  # take a http request from user
-    return render(request, 'todo/todo_list.html')  # returns the todolist page
+    items = Item.objects.all()
+    context = {
+        'items':  items
+    }
+    return render(request, 'todo/todo_list.html',context )  # returns the todolist page
+
 
 
 
