20633ffc0560b590638a081a7e26e0f12ac2e6a7
cwill83247 <breakthrumusic@hotmail.com>
2022-12-16 15:08:05 +0000

    Add: Adding Models for Item

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def say_hello(request): # take a http request from user
-    return HttpResponse("hello moto")    #returns a http resonse to user  neede to import http response at top  
+def get_todo_list(request):  # take a http request from user
+    return render(request, 'todo/todo_list.html')  # returns the todolist page
+
+
+
