8095a298afa86720db354a5d6a7818d39aa9774b
cwill83247 <breakthrumusic@hotmail.com>
2022-12-30 11:12:40 +0000

    creating local and live environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8&5ejc6fq@uthsuyi3$ek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]  
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 INSTALLED_APPS = [
